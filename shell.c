ddb8eb1fd113b05102b2f1ff307bb76f6ceb798a
MouaadELAMRANI <el.amrani.mouaad@gmail.com>
2023-10-17 21:02:11 +0100

    progress so far

--- shell.c
+++ shell.c
@@ -2,7 +2,7 @@
 
 int shell(void)
 {
-	char *cmd = NULL, *cmd_cpy = NULL, *token = NULL;
+	char *cmd = NULL, *token = NULL;
 	char *delim = " \n";
 	size_t n = 0;
 	int argc = 0, i = 0;
@@ -12,7 +12,6 @@ int shell(void)
 	if (getline(&cmd, &n, stdin) == -1)
 		return (-1);
 
-	cmd_cpy = strdup(cmd);
 	token = strtok(cmd, delim);
 	while (token)
 	{
@@ -22,7 +21,7 @@ int shell(void)
 	printf("%d\n", argc);
 
 	argv = malloc(sizeof(char *) * argc);
-	token = strtok(cmd_cpy, delim);
+	token = strtok(cmd, delim);
 
 	while (token)
 	{
@@ -39,7 +38,7 @@ int shell(void)
 		printf("%s\n", argv[i++]);
 	}
 
-	free(cmd), free(cmd_cpy), free(argv);
+	free(cmd), free(argv);
 
 	return (0);
 
