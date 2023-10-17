2bc87881feddccc37d36d3addd34013407e72f7b
MouaadELAMRANI <el.amrani.mouaad@gmail.com>
2023-10-17 21:52:58 +0100

    issue assigning arguments

--- shell.c
+++ shell.c
@@ -7,40 +7,51 @@ int shell(void)
 	size_t n = 0;
 	int argc = 0, i = 0;
 	char **argv = NULL;
+    char *cmdpath = NULL;
 
+    /* taking input */
 	printf("$ ");
 	if (getline(&cmd, &n, stdin) == -1)
 		return (-1);
 
+    // tokenizing input to count argc
 	token = strtok(cmd, delim);
+
+    /* counting argc */
 	while (token)
 	{
 		token = strtok(NULL, delim);
 		argc++;
 	}
-	printf("%d\n", argc);
 
-	argv = malloc(sizeof(char *) * argc);
+    printf("NUMBER OF ARGUMENTS: %d\n", argc);
+
+    /* assigning memory of size argc to argv */
+	argv = malloc(sizeof(char *) * argc - 1);
+
+    /* tokenizing input to assign values to argv */ 
 	token = strtok(cmd, delim);
 
+    /* assigning values to argv */
 	while (token)
 	{
 		argv[i] = token;
 		token = strtok(NULL, delim);
+        printf("ARGUMENT INDEX IS: %d\n", i);
+        printf("ARGUMENT VALUE IS: %s\n", argv[i]);
 		i++;
+        printf("AFTER WHILE I = %d\n", i);
 	}
+
+    /* ending argv array */ 
 	argv[i] = NULL;
 
-	i = 0;
+    /* executing command with arguments in environment */ 
+    execve(argv[0], argv, __environ);
 
-	while (argv[i])
-	{
-		printf("%s\n", argv[i++]);
-	}
+    /* freeing used resources */ 
+	free(cmd), free(argv); free(cmdpath);
 
-	free(cmd), free(argv);
-
+    /* finish */ 
 	return (0);
-
-
 }
