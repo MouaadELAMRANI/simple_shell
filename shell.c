88be323491344d0438b7eab517b82884e4230e4c
MouaadELAMRANI <el.amrani.mouaad@gmail.com>
2023-10-18 21:27:26 +0100

    handle ctrl+d

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <errno.h>
 
 int shell(void)
 {
@@ -9,76 +10,89 @@ int shell(void)
 	char **argv = NULL;
     extern char **environ;
 
-    printf("environ= %s\n", *environ);
     /* taking input */
 	printf("$ ");
 
 
 	if (getline(&cmd, &n, stdin) < 1)
     {
-        free(cmd);
-		return(-1);
-    }
-
-    printf("cmd: %s\n", cmd);
-   
-    cmd_dup = strdup(cmd);
-
-    /* tokenizing input to count argc */ 
-    token = strtok(cmd, delim);
-
-    /* counting argc */
-    while (token)
-    {
-        token = strtok(NULL, delim);
-        argc++;
-    }
-
-    /* assigning memory of size argc to argv */
-    argv = malloc(sizeof(char *) * argc);
-
-    /* tokenizing input to assign values to argv */ 
-    token = strtok(cmd_dup, delim);
-
-    /* assigning values to argv */
-    while (token)
-    {
-        argv[i] = token;
-        token = strtok(NULL, delim);
-        i++;
-    }
-
-    /* ending argv array */ 
-    argv[i] = NULL;
-
-    /* executing command with arguments in environment */ 
-    if (_strcmp(argv[0], "exit") == 0)
-    {
-        exit(0);
-    }
-    else if (access(argv[0], X_OK) == 0)
-    {
-        if (fork() == 0)
+        if (feof(stdin))
         {
-            execve(argv[0], argv, environ);
-            printf("$ ");
+            free(cmd);
+            exit(EXIT_SUCCESS);
         }
         else
         {
-            wait(&status);
+            free(cmd);
+            exit(EXIT_FAILURE);
         }
+    }
+    else if (_strlen(cmd) > 1) 
+    {
+        printf("cmd: %s\n", cmd);
+    
+        cmd_dup = strdup(cmd);
+
+        /* tokenizing input to count argc */ 
+        token = strtok(cmd, delim);
+
+        /* counting argc */
+        while (token)
+        {
+            token = strtok(NULL, delim);
+            argc++;
+        }
+
+        /* assigning memory of size argc to argv */
+        argv = malloc(sizeof(char *) * argc);
+
+        /* tokenizing input to assign values to argv */ 
+        token = strtok(cmd_dup, delim);
+
+        /* assigning values to argv */
+        while (token)
+        {
+            argv[i] = token;
+            token = strtok(NULL, delim);
+            i++;
+        }
+
+        /* ending argv array */ 
+        argv[i] = NULL;
+
+        /* executing command with arguments in environment */ 
+        if (_strcmp(argv[0], "exit") == 0)
+        {
+            exit(0);
+        }
+        else if (access(argv[0], X_OK) == 0)
+        {
+            if (fork() == 0)
+            {
+                execve(argv[0], argv, environ);
+                printf("$ ");
+            }
+            else
+            {
+                wait(&status);
+            }
+        }
+        else
+        {
+            printf("No such file or directory\n");
+        }
+
+        
+
+
+        /* freeing used resources */ 
+        free(cmd), free(argv);
+
+        /* finish */ 
+        return (0);
     }
     else
     {
-        printf("No such file or directory\n");
+        return (0);
     }
-
-    
-
-
-    /* freeing used resources */ 
-	free(cmd), free(argv);
-
-    /* finish */ 
-	return (0);
-}}
+}
